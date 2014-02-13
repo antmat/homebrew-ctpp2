d96f3d0c4e6569d1d201c550bf8c7773e66dd26e
Антон Матвеенко <antmat@MacBook-Pro-Anton-2.local>
2014-02-13 12:15:12 +0700

    adding patch. Formula fix

--- Formula/ctpp2.rb
+++ Formula/ctpp2.rb
@@ -5,12 +5,16 @@ class Ctpp2 < Formula
   url 'http://ctpp.havoc.ru/download/ctpp2-2.8.tar.gz'
   homepage 'http://ctpp.havoc.ru'
   md5 'f9474c6dae89b7c514a997d505c32e76'
-
   depends_on 'cmake' => :build
+#  depends_on 'gcc47'
+  fails_with [:llvm, :clang] do
+    cause <<-EOS.undent
+      Impossible to build with clang or llvm.
+      EOS
+  end
 
   def install
-    system "cmake #{std_cmake_parameters} ."
-#    system "make install" 
+    system "mkdir -p build && cd build && cmake -DCMAKE_CXX_COMPILER=`ls /usr/local/bin/g++* | tail -n1` #{std_cmake_parameters} .. && make install && cd .."
     include.install "include" => "ctpp2"
   end
 
